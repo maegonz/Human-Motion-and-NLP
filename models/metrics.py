530706fc6c25617a9f5d8a378ba3ae2cb07420ed
maegonz <manutonymanu@gmail.com>
2026-02-04 13:35:08 +0100

    Correct bleu implementation

--- models/metrics.py
+++ models/metrics.py
@@ -1,10 +1,11 @@
-from nltk.translate.bleu_score import sentence_bleu
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
 
 def score(reference: str, generated: str):
     """
     Compute BLEU score between a reference and a generated sentence.
     """
-    refs = [ref.split(' ') for ref in reference]
-    gen = generated.split(' ')
-    bleu_score = sentence_bleu(refs, gen)
+    refs = [reference.split()]
+    gen = generated.split()
+    smoothing = SmoothingFunction().method1
+    bleu_score = sentence_bleu(refs, gen, smoothing_function=smoothing)
     return bleu_score
